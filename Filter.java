812d1e64e1588a64a45d1caf8f5ffa24142f0a73
Zachary Chandos <zacharychandos.zc@gmail.com>
2024-01-30 21:41:40 -0600

    Assignment in progress

--- Filter.java
+++ Filter.java
@@ -18,7 +18,35 @@ public class Filter
    public static void main(String[] args)
    {
 
+      Scanner scanner = new Scanner(System.in);
 
+      int columnCount = 0;
+      int maxColumnCount = 0;
 
+      if (args.length > 0) {
+         try {
+             maxColumnCount = Integer.parseInt(args[0]);
+         } catch (NumberFormatException e) {
+             System.err.println("Invalid argument for number of columns. Using default.");
+         }
+     }
+
+      while (scanner.hasNextDouble()){
+         
+         double number = scanner.nextDouble();
+
+         System.out.printf("%16.13f  ", number);
+
+         columnCount++;
+
+         if(columnCount == maxColumnCount){
+            System.out.println();
+   
+            columnCount = 0;
+         }
+      
+      }
+
+      scanner.close();
    }
 }
